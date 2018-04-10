38b24e47e67248785f674d8271819b6a9c6c3990
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-10 16:46:34 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,79 @@
+INSERT INTO projects (
+  title, category, funding_goal, start_date, end_date)
+  VALUES ('College Tuition', 'Education', 60000, '4/1/18', '6/1/18');
+INSERT INTO projects (
+  title, category, funding_goal, start_date, end_date)
+  VALUES ('Pay My Rent', 'General', 30000, '6/1/18', '6/1/19');
+INSERT INTO projects (
+  title, category, funding_goal, start_date, end_date)
+  VALUES ('Hospital Bill', 'Urgent', 80000, '3/1/18', '7/1/18');
+INSERT INTO projects (
+  title, category, funding_goal, start_date, end_date)
+  VALUES ('Vacation', 'Fun', 10000, '6/1/18', '7/1/18');
+INSERT INTO projects (
+  title, category, funding_goal, start_date, end_date)
+  VALUES ('Wedding', 'Fun', 100000, '6/1/18', '12/1/18');
+INSERT INTO projects (
+  title, category, funding_goal, start_date, end_date)
+  VALUES ('Pet Vet Bill', 'Urgent', 10000, '4/1/18', '5/1/18');
+INSERT INTO projects (
+  title, category, funding_goal, start_date, end_date)
+  VALUES ('Coding Tuition', 'Education', 12000, '1/1/18', '1/1/19');
+INSERT INTO projects (
+  title, category, funding_goal, start_date, end_date)
+  VALUES ('Luxury Handbag Fund', 'Fun', 5000, '5/1/18', '6/1/18');
+INSERT INTO projects (
+  title, category, funding_goal, start_date, end_date)
+  VALUES ('Cancer Research', 'Science', 1000000, '5/1/18', '5/1/20');
+INSERT INTO projects (
+  title, category, funding_goal, start_date, end_date)
+  VALUES ('Lady Gaga Concert', 'Fun', 500, '5/1/18', '6/1/18');
+
+INSERT INTO users (
+    name, age)
+    VALUES ('Bob', 18);
+INSERT INTO users (
+    name, age)
+    VALUES ('Jim', 22);
+INSERT INTO users (
+    name, age)
+    VALUES ('Sarah', 18);
+INSERT INTO users (
+    name, age)
+    VALUES ('Jimmy', 30);
+INSERT INTO users (
+    name, age)
+    VALUES ('Maria', 33);
+INSERT INTO users (
+    name, age)
+    VALUES ('Elle', 21);
+INSERT INTO users (
+    name, age)
+    VALUES ('Sydney', 21);
+INSERT INTO users (
+    name, age)
+    VALUES ('Adrian', 54);
+INSERT INTO users (
+    name, age)
+    VALUES ('Lieve', 56);
+INSERT INTO users (
+    name, age)
+    VALUES ('Abby', 25);
+INSERT INTO users (
+    name, age)
+    VALUES ('John', 59);
+INSERT INTO users (
+    name, age)
+    VALUES ('Laura', 42);
+INSERT INTO users (
+    name, age)
+    VALUES ('Aurora', 33);
+INSERT INTO users (
+    name, age)
+    VALUES ('Polly', 60);
+INSERT INTO users (
+    name, age)
+    VALUES ('Robert', 29);
+INSERT INTO users (
+    name, age)
+    VALUES ('Morgan', 38);
